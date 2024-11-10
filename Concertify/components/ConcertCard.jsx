35500577fabf7d0c85d9c865fabd8a890fb0b822
maraaaqzz <maraalmasan@gmail.com>
2024-11-10 21:02:28 +0200

    concert details page when selected from all concerts

--- Concertify/components/ConcertCard.jsx
+++ Concertify/components/ConcertCard.jsx
@@ -1,16 +1,31 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import React from "react";
-import { useRouter } from 'expo-router'; 
+import { useRouter } from 'expo-router';
 
 export const ConcertCard = ({ item }) => {
     const router = useRouter();
 
     const navigateToConcertInfo = () => {
+        const concertDate = item.date.toDate(); 
+        const date = concertDate.toLocaleDateString('en-US', {
+            month: 'long',
+            day: 'numeric',
+            year: 'numeric',
+        });
+        const time = concertDate.toLocaleTimeString('en-US', {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true,
+        });
         router.push({
             pathname: '/concert', 
             params: {
                 name: item.name,
                 imageKey: item.imageKey,
+                photoUrl: item.photoUrl,
+                location: item.location,
+                date: date,
+                time: time
             },
         });
     };
@@ -19,7 +34,7 @@ export const ConcertCard = ({ item }) => {
         <TouchableOpacity onPress={navigateToConcertInfo} style={styles.cardContainer}>
           <Image
               style={styles.image}
-              source={item.image} // change with uri from database like source={{ uri: item.images[0].url }}
+              source={{uri:item.photoUrl}} 
           />
           <Text
               style={styles.title}
